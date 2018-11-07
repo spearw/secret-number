5b25a217c9a1d57e770aa36e06e342bf4f452b01
William Spear <spearw@gmail.com>
2018-11-07 10:05:04 -0500

    uses erb template and html

--- web_guesser.rb
+++ web_guesser.rb
@@ -2,9 +2,9 @@ require 'sinatra'
 require 'sinatra/reloader'
 
 
-secret_number = rand(101)
+number = rand(101)
 
 get '/frank-says' do
-    "The SECRET NUMBER is #{secret_number}"
+    erb :index, :locals => {:number => number}
 end
 
