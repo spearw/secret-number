20b5a8d1ef476d26435cc2a14c79782227ba02d3
William Spear <spearw@gmail.com>
2018-11-07 10:48:18 -0500

    correctly outputs if guess is too high or too low. Only displays secret number on correct guess

--- web_guesser.rb
+++ web_guesser.rb
@@ -2,9 +2,33 @@ require 'sinatra'
 require 'sinatra/reloader'
 
 
-number = rand(101)
+secret_number = rand(101)
+message = ""
 
 get '/frank-says' do
-    erb :index, :locals => {:number => number}
+    guess = params["guess"]
+    message = check_guess(guess, secret_number)
+    if message == "You got it right!"
+        win_message = "the SECRET NUMBER is #{secret_number}"
+        erb :index, :locals => {:win_message => win_message, :message => message}
+    else
+        erb :index, :locals => {:win_message => win_message, :message => message}
+    end
+end
+
+def check_guess(guess, secret_number)
+    if (params["guess"].to_i - 5) > secret_number
+        "WAY too high!"
+    elsif (params["guess"].to_i + 5) < secret_number
+        "WAY too low!"
+    elsif params["guess"].to_i > secret_number
+        "Too high!"
+    elsif params["guess"].to_i < secret_number
+        "Too low!"
+    elsif params["guess"].to_i == secret_number
+        "You got it right!"
+    else
+        false
+    end
 end
 
